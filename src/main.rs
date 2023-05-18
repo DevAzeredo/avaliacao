96a01c63986555b0b391cf6e1ba81130e8c58604
DevAzeredo <developer.azeredo@gmail.com>
2023-05-18 01:03:18 -0300

    18052023

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,76 @@
 use actix_files::NamedFile;
 use actix_web::{
+    error::InternalError,
     get,
     http::{Method, StatusCode},
     middleware, web, App, Either, HttpRequest, HttpResponse, HttpServer, Responder, Result,
 };
-use chrono::Local;
+use chrono::{DateTime, Local};
+use sailfish::TemplateOnce;
 use serde::{Deserialize, Serialize};
-use surrealdb::{
-    dbs::Session,
-    engine::{
-        any::{self, Any},
-        remote,
-        remote::ws::{Client, Ws}, local::{File, RocksDb},
-    },
-    kvs::Datastore,
-    Surreal,
-};
+use surrealdb::{engine::any::Any, opt::auth::Root, sql::Thing, Surreal};
+#[derive(TemplateOnce)]
+#[template(path = "index.stpl")]
+struct Index;
 
-use std::io;
+async fn default_handler() -> Result<impl Responder> {
+    let body = Index {}
+        .render_once()
+        .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(body))
+}
 
-async fn default_handler(req_method: Method) -> Result<impl Responder> {
-    match req_method {
-        Method::GET => {
-            let file = NamedFile::open(r"src\index.html")?
-                .customize()
-                .with_status(StatusCode::FOUND);
-            Ok(Either::Left(file))
+#[derive(TemplateOnce, Deserialize, Debug)]
+#[template(path = "relatorio.stpl")]
+struct Relatorio {
+    datainicio: String,
+    datafim: String,
+}
+
+async fn relatorio(req: HttpRequest) -> Result<impl Responder> {
+    let query_string = req.query_string();
+    let params: Vec<&str> = query_string.split('&').collect();
+
+    let mut datainicio = None;
+    let mut datafim = None;
+
+    for param in params {
+        let parts: Vec<&str> = param.split('=').collect();
+        if parts.len() != 2 {
+            continue;
         }
-        _ => Ok(Either::Right(HttpResponse::MethodNotAllowed().finish())),
+        match parts[0] {
+            "datainicio" => datainicio = Some(parts[1]),
+            "datafim" => datafim = Some(parts[1]),
+            _ => (),
+        }
+    }
+
+    if datainicio.is_none() || datafim.is_none() {
+        let body = Relatorio {
+            datainicio: "0".to_owned(),
+            datafim: "0".to_owned(),
+        }
+        .render_once()
+        .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+        Ok(HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(body))
+    } else {
+        let body = Relatorio {
+            datainicio: datainicio.unwrap().to_owned(),
+            datafim: datafim.unwrap().to_owned(),
+        }
+        .render_once()
+        .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+        Ok(HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(body))
     }
 }
+
 async fn bulma_css(req_method: Method) -> Result<impl Responder> {
     match req_method {
         Method::GET => {
@@ -41,7 +82,6 @@ async fn bulma_css(req_method: Method) -> Result<impl Responder> {
 }
 
 async fn all_css(req_method: Method) -> Result<impl Responder> {
-    println!("asdsadsa");
     match req_method {
         Method::GET => {
             let file = NamedFile::open(r"src\css\all.min.css")?.customize();
@@ -62,48 +102,45 @@ async fn web_fonts(req: HttpRequest) -> Result<impl Responder> {
         _ => Ok(Either::Right(HttpResponse::MethodNotAllowed().finish())),
     }
 }
-#[get("/avaliacao/{text}/{idBotaoSelecionado}")]
-async fn avaliacao(req: HttpRequest) -> Result<impl Responder> {
-    /* match req.method().to_owned() {
-        Method::GET => {
-
-            Ok(Either::Left(file))
-        }
-        _ => Ok(Either::Right(HttpResponse::MethodNotAllowed().finish())),
-    } */
-    let textarea = req.match_info().query("text");
-    let id_btn = req.match_info().query("idBotaoSelecionado");
-    let now: chrono::DateTime<Local> = Local::now();
-    return default_handler(req.method().to_owned()).await;
+#[derive(Debug, Serialize, Deserialize)]
+struct Avaliacao {
+    textarea: String,
+    nota: String,
 }
-#[derive(Debug, Serialize)]
-struct Avaliacao<'a> {
-    text: &'a str,
-    name: &'a str,
-    datetime: chrono::DateTime<Local>,
+#[derive(Debug, Deserialize)]
+struct Record {
+    #[allow(dead_code)]
+    id: Thing,
+}
+
+async fn avaliacao(req: HttpRequest) -> Result<impl Responder> {
+    println!("entrou aqui");
+
+    let created: Record = DB
+        .create("Avaliacao")
+        .content(Avaliacao {
+            textarea: req.match_info().query("text").to_string(),
+            nota: req.match_info().query("nota").to_string(),
+        })
+        .await
+        .unwrap();
+
+    return default_handler().await;
 }
 
 static DB: Surreal<Any> = Surreal::init();
-struct Person {
-    id: i32,
-    name: String,
-}
-
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("criado ");
-
-    println!("tentanu ");
+    println!("Iniciando...");
+    println!("Conectando ao banco...");
     DB.connect("file://db.db").await?;
-    println!("conectado");
 
-    log::info!("Conectando no banco");
+    DB.use_ns("namespace").use_db("database").await?;
+    let a: Vec<Avaliacao> = DB.select("Avaliacao").await.unwrap();
+    println!("{:?}", a);
+    println!("Conectado!");
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
-    //DB.connect("ws://localhost:8000").await.unwrap();
-    println!("asdsassssssssssssa");
-
-    log::info!("starting HTTP server at http://192.168.1.18:64555");
+    println!("starting HTTP server at http://192.168.1.18:64555");
 
     HttpServer::new(move || {
         App::new()
@@ -111,6 +148,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .wrap(middleware::Logger::default())
             .service(web::resource("/bulma").route(web::get().to(bulma_css)))
             .service(web::resource("/all").route(web::get().to(all_css)))
+            .service(web::resource("/relatorio").route(web::get().to(relatorio)))
+            .service(web::resource("/avaliacao/{text}/{nota}").route(web::get().to(avaliacao)))
             .service(web_fonts)
             // default
             .default_service(web::to(default_handler))
